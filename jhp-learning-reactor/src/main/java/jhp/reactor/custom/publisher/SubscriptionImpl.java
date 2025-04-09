bdd1d3d221d1a4a3b1de311a64f5161f16b09e07
JHPrk <qkrwogus1236@gmail.com>
2025-04-10 00:25:21 +0900

    Java Reactor 연습 First Commit. add readme file, publisher/subscriber 구현

---
+++ jhp-learning-reactor/src/main/java/jhp/reactor/custom/publisher/SubscriptionImpl.java
@@ -0,0 +1,43 @@
+package jhp.reactor.custom.publisher;
+
+import com.github.javafaker.Faker;
+import jhp.reactor.custom.subscriber.SubscriberImpl;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubscriptionImpl implements Subscription {
+  private static final Logger log = LoggerFactory.getLogger(SubscriberImpl.class);
+  private static final int MAX_ITEMS = 10;
+  private final Faker faker;
+  private final Subscriber<? super String> subscriber;
+  private boolean isCancelled;
+  private int count = 0;
+  public SubscriptionImpl(Subscriber<? super String> subscriber){
+    this.subscriber = subscriber;
+    this.faker = Faker.instance();
+  }
+  @Override
+  public void request(long requested) {
+    if (isCancelled){
+      return;
+    }
+    log.info("subscriber has requested {} items", requested);
+    for (int i = 0; i < requested && count < MAX_ITEMS; i ++){
+      count ++;
+      this.subscriber.onNext(this.faker.internet().emailAddress());
+    }
+    if (count == MAX_ITEMS){
+      log.info("no more data to produce");
+      subscriber.onComplete();
+      this.isCancelled = true;
+    }
+  }
+
+  @Override
+  public void cancel() {
+    log.info("subscriber has cancelled");
+    this.isCancelled = true;
+  }
+}
